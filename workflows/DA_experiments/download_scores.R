a7e29fb641861bd31ab45661d6c1279c305591fe
hlwander <hwander@vt.edu>
2023-05-19 11:22:39 -0400

    editing script that downloads scores so that there is only one scores folder

--- workflows/DA_experiments/download_scores.R
+++ workflows/DA_experiments/download_scores.R
@@ -1,6 +1,6 @@
 lake_directory <- here::here()
-download.file(url = "https://zenodo.org/record/7925098/files/scores.zip?download=1", destfile = file.path(lake_directory,"scores.zip"), method = "curl")
-unzip(file.path(lake_directory,"scores.zip") ,exdir = "scores")
+download.file(url = "https://zenodo.org/record/7925098/files/scores.zip?download=1", destfile = file.path(lake_directory,"archive/scores.zip"), method = "curl")
+unzip(file.path(lake_directory,"archive/scores.zip") ,exdir = "scores")
 
-download.file(url = "https://zenodo.org/record/7925098/files/targets.zip?download=1", destfile = file.path(lake_directory,"targets.zip"), method = "curl")
-unzip(file.path(lake_directory,"targets.zip") ,exdir = "targets")
+download.file(url = "https://zenodo.org/record/7925098/files/targets.zip?download=1", destfile = file.path(lake_directory,"archive/targets.zip"), method = "curl")
+unzip(file.path(lake_directory,"archive/targets.zip") ,exdir = "targets")
