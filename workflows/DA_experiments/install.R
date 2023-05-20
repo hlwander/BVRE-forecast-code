755fad7ee1d4b152f4c8f287ab7dfc5db36b1b39
rqthomas <rqthomas@vt.edu>
2023-05-20 19:32:02 +0000

    moving install.R

--- workflows/DA_experiments/install.R
+++ workflows/DA_experiments/install.R
@@ -1,9 +1,13 @@
-#load packages
-if (!require("pacman"))install.packages("pacman")
-pacman::p_load(curl, raster, lubridate, tidyverse, zoo, aws.s3, ggplot2,
-               ggpubr, egg, purrr, ggnewscale, viridis, stringr, readr, remotes, verification,
-               here) 
-
+install.packages("curl")
+install.packages("tidyverse")
+install.packages("zoo")
+install.packages("ggubrr")
+install.packages("egg")
+install.packages("ggnewscale")
+install.packages("viridis")
+install.packages("remotes")
+install.packages("verification")
+install.packages("here")
 remotes::install_github("SwampThingPaul/AnalystHelper")
 remotes::install_github("FLARE-forecast/GLM3r", ref = "a57f41d9e8e9a12e72675f2e02f032fe9f27b11c") 
 remotes::install_github("rqthomas/glmtools", ref = "b50e9a7b73e41afcd8119e2b9ac172c1c7beb51f")
